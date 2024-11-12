90cf9651c65e8ab29c6e8de5f2dd88d7864f7e5b
GaspLEV <gaspar.leveque@gmail.com>
2024-11-12 17:42:26 +0100

    freeze réseau sauf dernière couche OK

--- recup_resnet18.m
+++ recup_resnet18.m
@@ -1,22 +1,10 @@
-% Access the trained model
-[net, classes]= imagePretrainedNetwork("resnet18");
-
 % See details of the architecture
+net = net_2;
 net.Layers
+
+%freeze tout le réseau sauf la dernière couche qui apprend (head)
+[layerName,learnableNames] = networkHead(net)
+net = freezeNetwork(net,LayerNamesToIgnore=layerName);
+
 deepNetworkDesigner(net)
 
-% Read the image to classify
-I = imread('peppers.png');
-
-% Adjust size of the image
-sz = net.Layers(1).InputSize;
-I = I(1:sz(1),1:sz(2),1:sz(3));
-
-% Classify the image using ResNet-18
-scores = predict(net, single(I));
-label = scores2label(scores, classes)
-
-% Show the image and the classification results
-figure
-imshow(I)
-text(10,20,char(label),'Color','white')
